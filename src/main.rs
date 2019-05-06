8f818a0408bb7aa8a26711764567d80c170aa34b
Daniel Gilchrist <dan@tanda.co>
2019-05-06 17:51:52 +0100

    Cleaned up code and added asynchronous stdin

--- src/main.rs
+++ src/main.rs
@@ -3,58 +3,89 @@ extern crate termion;
 mod entities;
 use entities::player::{Player};
 
-use termion::input::TermRead;
-use termion::raw::IntoRawMode;
-use termion::event::Key;
+use std::io::{Write, Stdout, stdout};
+use std::time::{Duration};
+use std::thread::{sleep};
+
+use termion::{AsyncReader, async_stdin};
+use termion::input::{TermRead};
+use termion::raw::{IntoRawMode, RawTerminal};
+use termion::event::{Key};
 use termion::cursor::{Goto, Hide};
 use termion::clear::{All as ClearAll};
 use termion::style::{Reset};
-use std::io::{Write, Stdout, stdout, stdin};
 
-fn main() {
-  let stdin = stdin();
-  let mut stdout = stdout().into_raw_mode().unwrap();
-  let (x, y) = termion::terminal_size().unwrap();
-  let x_pos: u16 = x / 2;
-  let y_pos: u16 = y - (y / 8);
-  let mut player = Player::new(x_pos, y_pos);
+struct Game {
+  stdin: AsyncReader,
+  stdout: RawTerminal<Stdout>,
+  player: Player,
+}
 
-  setup(&player, &mut stdout);
+impl Game {
+  pub fn new() -> Game {
+    let stdin = async_stdin();
+    let stdout = stdout().into_raw_mode().unwrap();
+    let (x, y) = termion::terminal_size().unwrap();
+    let x_pos: u16 = x / 2;
+    let y_pos: u16 = y - (y / 8);
+    let player = Player::new(x_pos, y_pos);
 
-  for c in stdin.keys() {
-    match c.unwrap() {
-      Key::Char('q') => break,
-      Key::Left      => player.move_left(),
-      Key::Right     => player.move_right(),
-      _              => ()
-    };
-
-    player.draw(&mut stdout);
+    Game {
+      stdin,
+      stdout,
+      player,
+    }
   }
 
-  reset(&mut stdout);
+  fn setup(&mut self) {
+    write!(
+      self.stdout,
+      "{}{}{}",
+      ClearAll,
+      Goto(self.player.x_pos(), self.player.y_pos()),
+      Hide
+    ).unwrap();
+
+    self.stdout.flush().unwrap();
+
+    self.player.draw(&mut self.stdout);
+  }
+
+  fn reset(&mut self) {
+    write!(
+      self.stdout,
+      "{}{}{}",
+      ClearAll,
+      Reset,
+      Goto(1, 1),
+    ).unwrap();
+  }
+
+  fn main_loop(&mut self) {
+    let stdin = &mut self.stdin;
+
+    loop {
+      // keeps shit smoooooth
+      sleep(Duration::from_millis(10));
+
+      let key = stdin.keys().next();
+      match key {
+        Some(Ok(Key::Char('q'))) => break,
+        Some(Ok(Key::Left))      => self.player.move_left(),
+        Some(Ok(Key::Right))     => self.player.move_right(),
+        None                 => (),
+        _                    => ()
+      };
+
+      self.player.draw(&mut self.stdout);
+    }
+  }
 }
 
-fn setup(player: &Player, stdout: &mut Stdout) {
-  write!(
-    stdout,
-    "{}{}{}",
-    ClearAll,
-    Goto(player.x_pos(), player.y_pos()),
-    Hide
-  ).unwrap();
+fn main() {
+  let mut game = Game::new();
 
-  stdout.flush().unwrap();
-
-  player.draw(stdout);
-}
-
-fn reset(stdout: &mut Stdout) {
-  write!(
-    stdout,
-    "{}{}{}",
-    ClearAll,
-    Reset,
-    Goto(1, 1),
-  ).unwrap();
+  game.setup();
+  game.main_loop();
+  game.reset();
 }
