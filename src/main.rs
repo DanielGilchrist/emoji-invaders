64602f48419ed82bdbec0f1a79f145ae97872275
DanielGilchrist <danniieelg@gmail.com>
2019-10-06 14:11:34 +0100

    Moved arrows in match to align with other arrows

--- src/main.rs
+++ src/main.rs
@@ -71,8 +71,8 @@ impl Game {
         Some(Ok(Key::Char('q'))) => break,
         Some(Ok(Key::Left))      => self.player.move_left(),
         Some(Ok(Key::Right))     => self.player.move_right(),
-        None                 => (),
-        _                    => ()
+        None                     => (),
+        _                        => ()
       };
 
       self.player.draw(&mut self.stdout);
